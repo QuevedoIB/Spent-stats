292276cba8e0fea2227d5032469b7a2cdb4349dd
Ivan <4esoquevedoi@gmail.com>
2021-11-28 00:00:06 +0100

    Setup initial layout and navbar

---
+++ src/components/common/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Image src="/assets/logo-spent-stats.png" alt="logo" width="200" height="80" />
+        </nav>
+    )
+}
+
+export default Navbar
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import { SessionProvider } from "next-auth/react";
 import { appWithTranslation } from "next-i18next";
 
+import Navbar from "../components/common/Navbar/index.tsx";
+
+import "../styles/globals.css";
+
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   console.log({ session });
   return (
     <SessionProvider session={session}>
+      <Navbar />
       <Component {...pageProps} />
     </SessionProvider>
   );
